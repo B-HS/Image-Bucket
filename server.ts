e34563222e654296002c82be9c7554d7f4c12eaa
Hyunseok Byun <gumyoincirno@gmail.com>
2024-11-02 20:43:29 +0900

    Add logic for convert to webp/thumbnail with original file

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { Hono } from 'hono'
 import { env } from 'hono/adapter'
-import { cors } from 'hono/cors'
 import { stream } from 'hono/streaming'
 import sharp = require('sharp')
 
@@ -19,74 +18,81 @@ const saveImageWithGeneratedThumbnail = async (file: File, folderPath: string, p
     if (!ext || !imageExtensions.includes(ext)) {
         return { path, thumbnail: '' }
     }
-
     writeFileSync(path, new Uint8Array(fileBuffer))
+
+    await sharp(fileBuffer)
+        .metadata()
+        .then((info) => {
+            const width = info.width && Math.min(800, info.width)
+            return sharp(fileBuffer).resize(width).webp({ quality: 80 }).toBuffer()
+        })
+        .then((output) => writeFileSync(`${folderPath}/${fileName}.webp`, new Uint8Array(output)))
+
     await sharp(fileBuffer)
         .metadata()
         .then((info) => {
             const width = info.width && Math.round((info.width * percentage) / 100)
             const height = info.height && Math.round((info.height * percentage) / 100)
-            return sharp(fileBuffer).resize(width, height).toBuffer()
+            return sharp(fileBuffer).resize(width, height).webp({ quality: 80 }).toBuffer()
         })
         .then((output) => writeFileSync(`${folderPath}/${fileName}-thumbnail.webp`, new Uint8Array(output)))
-    return { path, thumbnail: `${folderPath}/${fileName}-thumbnail.webp` }
+
+    const responseData = { path: `${fileName}.webp`, thumbnail: `${folderPath}/${fileName}-thumbnail.webp` }
+    return responseData
 }
 
 app.get('/:imagename', async (c) => {
-    const Env = env(c)
-    const folderPath = (Env.folder_path as string) || '/Desktop'
-    const imagename = c.req.param('imagename')
-    const isThumbnail = c.req.query('thumbnail')
-    const path = isThumbnail ? `${folderPath}/${imagename.split('.')[0]}-thumbnail.webp` : `${folderPath}/${imagename}`
-    const image = readFileSync(path)
+    try {
+        const Env = env(c)
+        const folderPath = (Env.folder_path as string) || '/Desktop'
+        const imagename = c.req.param('imagename')
+        const isThumbnail = c.req.query('thumbnail')
+        const path = isThumbnail ? `${folderPath}/${imagename.split('.')[0]}-thumbnail.webp` : `${folderPath}/${imagename}`
+        const image = readFileSync(path)
 
-    return stream(c, async (stream) => {
-        await stream.write(new Uint8Array(image)).then(() => stream.close())
-    })
-})
+        if (!image) {
+            return c.text('Image not found', 404)
+        }
 
-app.post('/image/upload/single', async (c) => {
-    const Env = env(c)
-    const folderPath = (Env.folder_path as string) || '/Desktop'
-    const formData = await c.req.parseBody()
-    const percentage = 25
-    const file = formData['file']
-    if (file instanceof File) {
-        return c.json({
-            image: saveImageWithGeneratedThumbnail(file, folderPath, percentage),
+        return stream(c, async (stream) => {
+            await stream.write(new Uint8Array(image)).then(() => stream.close())
         })
-    } else {
-        return c.text('Invalid file', 400)
+    } catch (error) {
+        console.log(error)
+        return c.text('Failed to request', 500)
     }
 })
 
-app.post('/image/upload/multiple', async (c) => {
-    const Env = env(c)
-    const folderPath = (Env.folder_path as string) || '/Desktop'
-    const formData = await c.req.parseBody()
-    const percentage = 25
-    const files = formData['file']
-    if (Array.isArray(files)) {
+app.post('/image/upload', async (c) => {
+    try {
+        const Env = env(c)
+        const folderPath = (Env.folder_path as string) || '/Desktop'
+        const formData = await c.req.parseBody()
+        const percentage = 35
+        const files = formData['file']
+        const fileList = Array.isArray(files) ? files : [files]
+
         const images = []
-        for (const file of files) {
+        for (const file of fileList) {
             if (file instanceof File) {
                 const fileBuffer = await file.arrayBuffer()
                 const fullName = file.name
                 const ext = fullName.split('.').pop()
                 const path = `${folderPath}/${fullName.split('.')[0]}.${ext}`
                 writeFileSync(path, new Uint8Array(fileBuffer))
-                images.push(saveImageWithGeneratedThumbnail(file, folderPath, percentage))
+                images.push(await saveImageWithGeneratedThumbnail(file, folderPath, percentage))
             }
         }
         return c.json({
             images,
         })
-    } else {
-        return c.text('Invalid files', 400)
+    } catch (error) {
+        console.log(error)
+        return c.text('Failed to request', 500)
     }
 })
 
 export default {
-    port: 10001,
+    port: 20002,
     fetch: app.fetch,
 }
